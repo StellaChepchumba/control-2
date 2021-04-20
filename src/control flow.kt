87a5f5c3e27b2dec9de9a34e7f8dbfd2151c1ce8
stella4545 <stellachepchumbah@gmail.com>
2021-04-20 22:11:42 +0300

    first commit

---
+++ src/control flow.kt
@@ -0,0 +1,45 @@
+fun main(){
+   println( IsEven(4,))
+    println(IsEven(3))
+    println(IsEven(7))
+    println(IsEven(5))
+
+
+}
+fun IsEven(num:Int):Boolean {
+    if (num % 2 == 0) {
+        return true
+    } else if (num % 3 == 0) {
+        return false
+    } else {
+        return false
+    }
+    fun productList(product: String) {
+        fun product(name: String, weight: Int, price: Int, category: String) {
+            var category = arrayOf(weight, price, category)
+            when (category) {
+            }
+        }
+
+        fun Bring(name: String) :String {
+            var x = ""
+            for (items in name) {
+                if (name.indexOf(items) % 2 == 0) {
+                    println(items)
+                }
+            }
+            return "${x}"
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
